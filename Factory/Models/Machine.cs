7077b7f62613b744081cc2be80a323063fbd722f
Anthony DiFalco <aodifalco@gmail.com>
2022-08-05 10:59:04 -0700

    update add engineer and create views for Machine class

--- Factory/Models/Machine.cs
+++ Factory/Models/Machine.cs
@@ -10,6 +10,7 @@ namespace Factory.Models
     }
 
     public int MachineId { get; set; }
+    public string Name { get; set; }
     public string Description { get; set; }
     public virtual ICollection<Repairs> Engineers { get; set; }
   }
